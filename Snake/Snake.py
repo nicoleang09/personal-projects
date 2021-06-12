fb593c3d1f980fdfbd3982eca991bd610ac69a7e
nicoleang09 <nicoleang09@gmail.com>
2021-06-12 21:50:40 +0800

    Improve coding style

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -2,20 +2,21 @@ import curses
 from random import randint
 
 # window setup
-curses.initscr()
 rows = 20
 cols = 60
+
+curses.initscr()
 win = curses.newwin(rows, cols, 0, 0)
 win.keypad(1)
-curses.noecho()
-curses.curs_set(0)
 win.border(0)
 win.nodelay(True)
+curses.noecho()
+curses.curs_set(0)
 
 
 # data
 snake = [(10, 30), (9, 30), (8, 30), (7, 30), (6, 30)] # store snake info in (y, x) format
-food = (randint(1, rows - 2), randint(1, cols - 2))
+food = (randint(1, rows - 2), randint(1, cols - 2)) # generate position of food
 
 
 # logic
@@ -23,7 +24,7 @@ score = 0
 win.addch(food[0], food[1], '#') # show food
 
 ESC = 27 # define ESC as esc key no.
-key = curses.KEY_DOWN # snake will start by moving right
+key = curses.KEY_DOWN # snake will start by moving down
 
 while key != ESC:
 
@@ -32,10 +33,10 @@ while key != ESC:
 
     prev_key = key
     event = win.getch()
-    key = event if event != -1 else prev_key
+    key = event if event != -1 else prev_key # if no key pressed then no change
 
     if key not in [curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_UP, curses.KEY_RIGHT, ESC]:
-        key = prev_key
+        key = prev_key # if invalid key then no change
 
     # get coordinates of snake head
     y = snake[0][0]
@@ -62,10 +63,10 @@ while key != ESC:
     # check if snake ate food
     if snake[0] == food:
         score += len(snake)
-        food = ()
+        food = () # no more food
 
         while food == ():
-            food = (randint(1, rows - 2), randint(1, cols - 2))
+            food = (randint(1, rows - 2), randint(1, cols - 2)) # generate new food
 
             if food in snake:
                 food = ()
